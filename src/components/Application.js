3b3179a7281544175f14053bc148cc024573f4ff
Ssophia <susu94827@gmail.com>
2023-01-17 00:49:07 -0500

    bookinterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -24,35 +24,30 @@ export default function Application(props) {
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, [])
-  
-  // const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  // const appointmentList = dailyAppointments.map(appointment =>
-  //   <Appointment
-  //     key={appointment.id}
-  //     {...appointment}
-  //   />
-  // )
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+  const bookInterview=function (id, interview) {
+    console.log(id, interview);
+  }
 
-const dailyAppointments = getAppointmentsForDay(state, state.day);
-const interviewers = getInterviewersForDay(state, state.day);
+  const appointmentList = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
 
-const appointmentList = dailyAppointments.map((appointment) => {
-  const interview = getInterview(state, appointment.interview);
-
-  return (
-    <Appointment
-      key={appointment.id}
-      id={appointment.id}
-      time={appointment.time}
-      interview={interview}
-      interviewers={interviewers}
-    />
-  );
-});
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,11 +10,21 @@ import Form from "./Form";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+
+
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  const save = function(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+  }
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -29,6 +39,7 @@ export default function Appointment(props) {
         <Form
           interviewer={props.interviewer}
           interviewers={props.interviewers}
+          onSave={save}
           onCancel={() => back()}
         />)}
     </article>
